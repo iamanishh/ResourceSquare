45827f009531a1010a2ef7d82e5d3099e858e741
Manish Kumar <mmm.manish163@gmail.com>
2021-05-24 18:37:57 +0530

    first commit

---
+++ Index file/js/main.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+
+const navbarLinks = document.getElementsByClassName("nav__list")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
